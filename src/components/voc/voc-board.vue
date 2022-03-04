8054e5155c120117452d219265c4f8abfcc22e76
park <bbch1213@gmail.com>
2022-03-04 15:54:43 +0900

    테이블 설정

--- src/components/voc/voc-board.vue
+++ src/components/voc/voc-board.vue
@@ -1,6 +1,27 @@
 <template>
   <div>
-
+    <table>
+      <thead>
+      <tr>
+        <th scope="col">번호</th>
+        <th hidden scope="col">id</th>
+        <th scope="col">제목</th>
+        <th scope="col">고객 ID</th>
+        <th scope="col">답변상태</th>
+        <th scope="col">작성일자</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="(item, index) in list" :key="index">
+        <td scope="row">{{index+1}}</td>
+        <td hidden scope="row">{{item.id}}</td>
+        <td scope="row">{{item.title}}</td>
+        <td scope="row">{{item.customerId}}</td>
+        <td scope="row">{{item.vocStatus}}</td>
+        <td scope="row">{{item.createdAt}}</td>
+      </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -9,13 +30,34 @@ import {VocVuex} from "../../store/voc/voc.store";
 
 export default {
   name: "voc-board",
+  data() {
+    return {
+      index: 0,
+      list: [{
+        index: '',
+        id: '',
+        title: '',
+        vocStatus: '',
+        customerId: '',
+        createdAt: '',
+      },]
+    }
+  },
   created() {
     if(sessionStorage.getItem('X-AUTH-TOKEN') === null)
-      VocVuex.dispatch("getListToServerByCustomerId", 'customerA').then(() => alert("!"));
-    else{
-      VocVuex.dispatch("getListToServerByAll").then(() => alert("@"));
+    {
+      VocVuex.dispatch("getListToServerByCustomerId", 'customerA').then(() => {
+        this.list = VocVuex.getters.getList;
+        console.log(this.list);
+      });
     }
-  }
+    else{
+      VocVuex.dispatch("getListToServerByAll").then(() => {
+        this.list = VocVuex.getters.getList;
+        console.log(this.list);
+      });
+    }
+  },
 }
 </script>
 
